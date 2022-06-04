ac2b9f5f87775fba56c3e222e2b5a31fd09f5f7a
Andrey Silva Lima <andreysl2001@gmail.com>
2022-06-04 19:43:01 -0300

    Método .catch() adicionado

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const btjoke = document.getElementById("btjoke").addEventListener("click", () =>
     fetch("https://api.chucknorris.io/jokes/random")
     .then((response) => response.json())
     .then((data) => displayJoke(data))
+    .catch(() => displayErro())
 
     function displayJoke(data){
         document.getElementById("joke").innerText = data.value
@@ -11,5 +12,9 @@ const btjoke = document.getElementById("btjoke").addEventListener("click", () =>
         setTimeout(() => {
             img.classList.remove("shake")
         }, 2000)
+    }
+
+    function displayErro(){
+        alert("Algum erro ocorreu ou a API está inoperante.")
     }
 })
